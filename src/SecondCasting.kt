a8dac2aefd8f451b41617722fa20e1c1eae0af43
jpalmat <jimmy.palma1990@gmail.com>
2020-07-24 19:08:10 -0500

    casting

---
+++ src/SecondCasting.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var castToInt: Int = 3.14.toInt()
+    var charToInt: Int = 'A'.toInt()
+    var intToChar: Char = 65.toChar()
+
+    println(castToInt)
+    println(charToInt)
+    println(intToChar)
+}
