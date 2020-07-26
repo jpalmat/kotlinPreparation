a1cb06a210e07db5af2caa2b6317da82dd779542
jpalmat <jimmy.palma1990@gmail.com>
2020-07-25 21:51:04 -0500

    function call function

--- src/SevenExtentionFunction.kt
+++ src/SevenExtentionFunction.kt
@@ -2,5 +2,10 @@ fun main(args: Array<String>) {
     infix fun String.toSpace(x: String): String = " $x"
 
     String().toSpace("x")
+
+    val test = makeMathFunction(5)
+
+    println(test(3))
 }
 
+fun makeMathFunction(num1: Int): (Int) -> Int = {num2 -> num1*num2}
