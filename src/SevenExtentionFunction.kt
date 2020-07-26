c52e9586fa9b1f12eb58153c50034024cd29c4af
jpalmat <jimmy.palma1990@gmail.com>
2020-07-25 22:02:58 -0500

    func as parameter

--- src/SevenExtentionFunction.kt
+++ src/SevenExtentionFunction.kt
@@ -4,8 +4,18 @@ fun main(args: Array<String>) {
     String().toSpace("x")
 
     val test = makeMathFunction(5)
-
     println(test(3))
+
+    val func1 = {num1: Int -> num1*2}
+    var intLis: List<Int> = listOf(1, 2, 3 ,4, 5)
+
+    makeOnList(intLis, func1)
 }
 
 fun makeMathFunction(num1: Int): (Int) -> Int = {num2 -> num1*num2}
+
+fun makeOnList(list: List<Int>, myFunc: (num: Int) -> Int){
+    for (i in list){
+        println(myFunc(i))
+    }
+}
